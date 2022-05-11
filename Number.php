4128d524c575bd1aad097ecd63474bbdcc30892d
clarkk <clark@electrobeat.dk>
2022-05-11 21:29:52 +0200

    added round_odd

--- Number.php
+++ Number.php
@@ -6,4 +6,14 @@ class Number {
 	static public function is_whole(float $num): bool{
 		return fmod($num, 1) == 0.0;
 	}
+	
+	static public function round_odd(float $n): int{
+		$r = round($n);
+		
+		if($r % 2){
+			return $r;
+		}
+		
+		return $n - $r >= 0 ? $r + 1 : $r - 1;
+	}
 }
